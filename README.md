26ca7c7173e867cce8803c9fe68c82e8980ebc37
wang0805 <wh_wang@live.com>
2019-04-11 01:05:27 +0800

    added body-parser as midware

---
+++ README.md
@@ -0,0 +1,10 @@
+POST has been tested using postman
+
+Outputs all in json file as if an api endpoint
+
+jsonfile used as its the simplest form of db
+
+update the validator
+did not amend blockHeight as unsure what is the requirement for that
+voting power is being added for each similar address&&pub_key.value
+all fields are required
--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const jsonfile = require("jsonfile");
+const bodyParser = require("body-parser");
 
 const app = express();
 const file = "sample.json";
 
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true
-  })
-);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("testing if works");
